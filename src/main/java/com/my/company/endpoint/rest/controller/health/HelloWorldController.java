3cadc45efeb46c17cf9b3ed1267840cbd0fdd8c2
juninho <hei.juninho.2@gmail.com>
2025-06-11 23:09:51 +0300

    fix: problem for persistent memories

--- src/main/java/com/my/company/endpoint/rest/controller/health/HelloWorldController.java
+++ src/main/java/com/my/company/endpoint/rest/controller/health/HelloWorldController.java
@@ -9,18 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.my.company.service.HelloWorldService;
 
-//@PojaGenerated
+@PojaGenerated
 @RestController
 @AllArgsConstructor
 public class HelloWorldController {
     private final HelloWorldService service;
 
-//    public static final ResponseEntity<String> OK = new ResponseEntity<>("OK", HttpStatus.OK);
-//    public static final ResponseEntity<String> KO =
-//            new ResponseEntity<>("KO", HttpStatus.INTERNAL_SERVER_ERROR);
+    public static final ResponseEntity<String> OK = new ResponseEntity<>("OK", HttpStatus.OK);
+    public static final ResponseEntity<String> KO =
+            new ResponseEntity<>("KO", HttpStatus.INTERNAL_SERVER_ERROR);
 
     @GetMapping("/hello")
-    public String helloWorld() {
+    public String helloWorld(@RequestParam String name) {
         return service.uploadHelloWorldMessage(name);
     }
 }
