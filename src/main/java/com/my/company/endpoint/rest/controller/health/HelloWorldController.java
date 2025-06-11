63ec6c36075b02e63c20e36dd9553e8610a1a3a9
juninho <hei.juninho.2@gmail.com>
2025-06-11 23:25:10 +0300

    fix: for CI format

--- src/main/java/com/my/company/endpoint/rest/controller/health/HelloWorldController.java
+++ src/main/java/com/my/company/endpoint/rest/controller/health/HelloWorldController.java
@@ -10,15 +10,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.my.company.service.HelloWorldService;
 
-@PojaGenerated
+//@PojaGenerated
 @RestController
 @AllArgsConstructor
 public class HelloWorldController {
     private final HelloWorldService service;
 
-    public static final ResponseEntity<String> OK = new ResponseEntity<>("OK", HttpStatus.OK);
-    public static final ResponseEntity<String> KO =
-            new ResponseEntity<>("KO", HttpStatus.INTERNAL_SERVER_ERROR);
+//    public static final ResponseEntity<String> OK = new ResponseEntity<>("OK", HttpStatus.OK);
+//    public static final ResponseEntity<String> KO =
+//            new ResponseEntity<>("KO", HttpStatus.INTERNAL_SERVER_ERROR);
 
     @GetMapping("/hello")
     public String helloWorld(@RequestParam String name) {
