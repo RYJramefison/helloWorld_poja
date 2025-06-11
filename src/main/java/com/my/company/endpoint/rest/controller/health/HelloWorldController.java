ab3dee268465e89d8f800cab68e54496eb1935d5
juninho <hei.juninho.2@gmail.com>
2025-06-11 16:40:39 +0300

    fix: hello world method

--- src/main/java/com/my/company/endpoint/rest/controller/health/HelloWorldController.java
+++ src/main/java/com/my/company/endpoint/rest/controller/health/HelloWorldController.java
@@ -13,6 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+    public static final ResponseEntity<String> OK = new ResponseEntity<>("OK", HttpStatus.OK);
+    public static final ResponseEntity<String> KO =
+            new ResponseEntity<>("KO", HttpStatus.INTERNAL_SERVER_ERROR);
+
     @GetMapping("/hello")
     public String helloWorld() {
         return "... world!";
