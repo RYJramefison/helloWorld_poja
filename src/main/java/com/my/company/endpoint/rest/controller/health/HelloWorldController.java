ccfa591519678c6d81093101a24922dba9f4c2f9
juninho <hei.juninho.2@gmail.com>
2025-06-12 08:18:47 +0300

    Fix: with format.bat

--- src/main/java/com/my/company/endpoint/rest/controller/health/HelloWorldController.java
+++ src/main/java/com/my/company/endpoint/rest/controller/health/HelloWorldController.java
@@ -1,27 +1,23 @@
 package com.my.company.endpoint.rest.controller.health;
 
-import com.my.company.PojaGenerated;
-import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import com.my.company.service.HelloWorldService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-//@PojaGenerated
+// @PojaGenerated
 @RestController
 @AllArgsConstructor
 public class HelloWorldController {
-    private final HelloWorldService service;
+  private final HelloWorldService service;
 
-//    public static final ResponseEntity<String> OK = new ResponseEntity<>("OK", HttpStatus.OK);
-//    public static final ResponseEntity<String> KO =
-//            new ResponseEntity<>("KO", HttpStatus.INTERNAL_SERVER_ERROR);
+  //    public static final ResponseEntity<String> OK = new ResponseEntity<>("OK", HttpStatus.OK);
+  //    public static final ResponseEntity<String> KO =
+  //            new ResponseEntity<>("KO", HttpStatus.INTERNAL_SERVER_ERROR);
 
-    @GetMapping("/hello")
-    public String helloWorld(@RequestParam String name) {
-        return service.uploadHelloWorldMessage(name);
-    }
+  @GetMapping("/hello")
+  public String helloWorld(@RequestParam String name) {
+    return service.uploadHelloWorldMessage(name);
+  }
 }
